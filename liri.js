6ae4ce77c621fcc2b4f20cd3b4083e7dde118926
Caroline Cran <cranch.caroline@gmail.com>
2019-09-20 23:28:30 -0400

    added functions to liri.js file. testing and screenshots to follow

--- liri.js
+++ liri.js
@@ -12,11 +12,125 @@ var spotify = new Spotify(keys.spotify);
 
 
 
-// node liri.js concert-this <artist/band name here>
-
-// This will search the Bands in Town Artist Events API 
-// ("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp") for an artist and render the following information about each event to the terminal:
-
-// -Name of the venue
-// -Venue location
-// -Date of the Event (use moment to format this as "MM/DD/YYYY")
+// Helper function that gets the artist name
+var getArtistNames = function(artist) {
+    return artist.name;
+  };
+  
+// Song Search - Spotify API
+  var getSong = function(songName) {
+    if (songName === undefined) {
+      songName = "What's my age again";
+    }
+    spotify.search(
+      {
+        type: "track",
+        query: songName
+      },
+      function(err, data) {
+        if (err) {
+          console.log("Error occurred: " + err);
+          return;
+        }
+        var songs = data.tracks.items;
+        for (var i = 0; i < songs.length; i++) {
+          console.log(i);
+          console.log("Artist(s): " + songs[i].artists.map(getArtistNames));
+          console.log("Song: " + songs[i].name);
+          console.log("Preview Song: " + songs[i].preview_url);
+          console.log("Album Name: " + songs[i].album.name);
+          console.log("-----------------------------------");
+        }
+      }
+    );
+  };
+  
+// Artist Search - Bands in Town API
+  var getArtist = function(artist) {
+    var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+  
+    axios.get(queryURL).then(
+      function(response) {
+        var jsonData = response.data;
+  
+        if (!jsonData.length) {
+          console.log("No results found for " + artist);
+          return;
+        }
+        console.log("Upcoming concerts for " + artist + ":");
+        for (var i = 0; i < jsonData.length; i++) {
+          var show = jsonData[i];
+          console.log(
+            show.venue.city + " at " + show.venue.name + " on " +
+            moment(show.datetime).format("MM/DD/YYYY")
+          );
+        }
+      }
+    );
+  };
+  
+//Movie Search - OMBD API
+  var getMovie = function(movieName) {
+    if (movieName === undefined) {
+      movieName = "Mr Nobody";
+    }
+    var urlHit =
+      "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=full&tomatoes=true&apikey=trilogy";
+  
+    axios.get(urlHit).then(
+      function(response) {
+        var jsonData = response.data;
+        console.log("Title: " + jsonData.Title);
+        console.log("Year: " + jsonData.Year);
+        console.log("Rated: " + jsonData.Rated);
+        console.log("IMDB Rating: " + jsonData.imdbRating);
+        console.log("Country: " + jsonData.Country);
+        console.log("Language: " + jsonData.Language);
+        console.log("Plot: " + jsonData.Plot);
+        console.log("Actors: " + jsonData.Actors);
+        console.log("Rotten Tomatoes Rating: " + jsonData.Ratings[1].Value);
+      }
+    );
+  };
+  
+  // Function for running a command based on text file
+  var doWhatItSays = function() {
+    fs.readFile("random.txt", "utf8", function(error, data) {
+      console.log(data);
+      var dataArr = data.split(",");
+      if (dataArr.length === 2) {
+        pick(dataArr[0], dataArr[1]);
+      } else if (dataArr.length === 1) {
+        pick(dataArr[0]);
+      }
+    });
+  };
+  
+  // Function for determining which command is executed
+  var pick = function(caseData, functionData) {
+    switch (caseData) {
+    case "concert-this":
+      getArtist(functionData);
+      break;
+    case "spotify-this-song":
+      getSong(functionData);
+      break;
+    case "movie-this":
+      getMovie(functionData);
+      break;
+    case "do-what-it-says":
+      doWhatItSays();
+      break;
+    default:
+      console.log("LIRI doesn't know that");
+    }
+  };
+  
+  // Function which takes in command line arguments and executes correct function accordingly
+  var runThis = function(argOne, argTwo) {
+    pick(argOne, argTwo);
+  };
+  
+ 
+  runThis(process.argv[2], process.argv.slice(3).join(" "));
+  
